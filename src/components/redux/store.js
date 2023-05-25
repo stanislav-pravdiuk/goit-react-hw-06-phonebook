2c0a3eafe1eec9024f1b0b4855fe8ad9bb40e0a3
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-05-25 16:00:15 +0300

    add persist

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,4 +1,18 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { reducer } from "./reducer";
+
+// export const store = configureStore({reducer});
+
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { reducer } from "./reducer";
 
-export const store = configureStore({reducer});
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+export const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store);
