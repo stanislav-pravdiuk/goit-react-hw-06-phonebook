c76cb0d0cdb4f4956ddcb07da003c7ac456d17dc
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-05-31 10:58:16 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import { persistor, store } from 'components/redux/store';
+// import { persistor, store } from 'components/redux/store';
+import { store } from 'components/redux/store';
 import App from 'components/App';
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
