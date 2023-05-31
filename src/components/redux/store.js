a5614dba445bff488e00233096688ff0e03c1119
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-05-31 10:56:02 +0300

    закоментил персистор в индекс и стор

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,18 +1,18 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { reducer } from "./reducer";
-
-// export const store = configureStore({reducer});
-
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { reducer } from "./reducer";
 
-const persistConfig = {
-    key: 'root',
-    storage,
-};
+export const store = configureStore({reducer});
 
-const persistedReducer = persistReducer(persistConfig, reducer);
-export const store = configureStore({ reducer: persistedReducer });
-export const persistor = persistStore(store);
+// import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { reducer } from "./reducer";
+
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, reducer);
+// export const store = configureStore({ reducer: persistedReducer });
+// export const persistor = persistStore(store);
